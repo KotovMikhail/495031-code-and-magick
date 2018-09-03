d56f5b8960ea8c78026adc6b22d79e5c3e4433b1
Mikhail <mihail_kotov_86@mail.ru>
2018-09-03 09:01:05 +0300

    Отрисовал гистограмму

--- js/stat.js
+++ js/stat.js
@@ -7,16 +7,35 @@ var GAP = 50;
 var FONT_GAP = 16;
 var TEXT_HEIGHT = 50;
 var BAR_WIDTH = 40;
-var barHeight = CLOUD_HEIGHT - FONT_GAP - TEXT_HEIGHT;
+var barHeight = -150;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx, names) {
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_Y, CLOUD_Y * 2, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  //   MAX_BAR      BAR[I]
+  // ----------- = --------
+  //  barHeight       X
+
+  //  X = (barHeight * BAR[I]) / MAX_BAR
+  var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
 
@@ -24,6 +43,6 @@ window.renderStatistics = function (ctx, names) {
     ctx.fillStyle = 'black';
     ctx.fillText(names[i], CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_HEIGHT - FONT_GAP);
     ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ',' + Math.floor(255 - 42.5 * i) + ',0)';
-    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_Y * 2, BAR_WIDTH, barHeight);
+    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + GAP) * i, CLOUD_HEIGHT - TEXT_HEIGHT, BAR_WIDTH, (barHeight * times[i]) / maxTime);
   }
 };
